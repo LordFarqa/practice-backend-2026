3c883fdf6d1aad39d26056182e114d01f33c40dd
LordFarqa <zakovrmisha@gmail.com>
2026-03-17 22:38:55 +0700

    Новый сидер дял статусов

--- projects/database/seeders/BookingStatusesSeeder.php
+++ projects/database/seeders/BookingStatusesSeeder.php
@@ -5,19 +5,25 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\BookingStatus;
 use Illuminate\Database\Seeder;
-use Database\Factories\BookingStatusesFactory;
 
 class BookingStatusesSeeder extends Seeder
 {
     public function run(): void
     {
         $statuses = [
-            ['name' => 'Номер свободен'],
-            ['name' => 'Номер Занят'],
-            ['name'=> 'Заявка отменена']
+            ['id' => 1, 'name' => 'active'],
+            ['id' => 2, 'name' => 'cancelled_by_admin'],
+            ['id' => 3, 'name' => 'cancelled_by_user'],
+            ['id' => 4, 'name' => 'completed'],
+            ['id' => 5, 'name' => 'completed'],
         ];
-        foreach($statuses as $status){
-            BookingStatus::firstOrCreate($status);
+        
+        foreach ($statuses as $status) {
+            BookingStatus::updateOrCreate(
+                ['id' => $status['id']],
+                ['name' => $status['name']] 
+            );
         }
+        
     }
 }
